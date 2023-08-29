982d5e6964c594fb73fccd814212bbe4b0b4e0de
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-08-29 15:03:27 +0200

    prognosis updates

--- src/components/PageContent/PrognosisContent.jsx
+++ src/components/PageContent/PrognosisContent.jsx
@@ -1,9 +1,80 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Divider, Tabs, TabList, TabPanels, Tab, TabPanel, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 
 const PrognosisContent = () => {
+  const mockData = [
+    {
+      day: 'Day 1',
+      weatherIcon: '🌞',
+      temperature: '25°C',
+      wind: '5km/h',
+      humidity: '20%',
+    },
+    {
+      day: 'Day 2',
+      weatherIcon: '☁️',
+      temperature: '22°C',
+      wind: '3km/h',
+      humidity: '22%',
+    },
+    {
+      day: 'Day 3',
+      weatherIcon: '🌦',
+      temperature: '23°C',
+      wind: '4km/h',
+      humidity: '18%',
+    },
+  ];
+
   return (
     <Box p={4}>
-      <Text fontSize="2xl">prognosis</Text>
+      <Text fontSize="2xl" mb={4}>
+        Prognosis
+      </Text>
+      <Divider mb={4} />
+      <Tabs isLazy align="center">
+        <TabList>
+          <Tab>Cards</Tab>
+          <Tab>Table</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            {mockData.map((data, index) => (
+              <Box key={index} bg="gray.200" p={4} borderRadius="md" mb={4} textAlign="center">
+                <Text fontSize="xl">{data.day}</Text>
+                <Text fontSize="4xl">{data.weatherIcon}</Text>
+                <Text>{data.temperature}</Text>
+                <Text>Wind: {data.wind}</Text>
+                <Text>Humidity: {data.humidity}</Text>
+              </Box>
+            ))}
+          </TabPanel>
+          <TabPanel>
+            <Table variant="simple" size="md">
+              <Thead>
+                <Tr>
+                  <Th>Day</Th>
+                  <Th>Weather</Th>
+                  <Th>Temperature</Th>
+                  <Th>Wind</Th>
+                  <Th>Humidity</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {mockData.map((data, index) => (
+                  <Tr key={index}>
+                    <Td>{data.day}</Td>
+                    <Td>{data.weatherIcon}</Td>
+                    <Td>{data.temperature}</Td>
+                    <Td>{data.wind}</Td>
+                    <Td>{data.humidity}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
