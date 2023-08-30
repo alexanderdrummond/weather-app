e8c2330236243bbc772df997610439a9e7cfa7e2
simo994q <simonkn1@hotmail.com>
2023-08-30 14:21:07 +0200

    home responsive

--- src/components/PageContent/Home/atoms/HomeFacts.jsx
+++ src/components/PageContent/Home/atoms/HomeFacts.jsx
@@ -1,36 +1,49 @@
-import { Grid, GridItem, Box, Text, Image, Flex } from '@chakra-ui/react';
+import { Grid, GridItem, Box, Text, Image, Flex, useMediaQuery } from '@chakra-ui/react';
 
 export function HomeFacts() {
+
+
+    const [isDesktop] = useMediaQuery('(min-width: 1200px)')
+
+
     return (
         <>
-            <Grid gridTemplateColumns='repeat(2, 1fr)' gap={4}>
+            <Grid gridTemplateColumns={isDesktop ? 'repeat(4, 1fr)' : 'repeat(2, 1fr)'} gap={4}>
                 <GridItem boxSizing='border-box' width="100%" textColor="white" m='0px 0px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-                    <Flex alignItems='center' justifyContent='space-between'>
-                        <Image src='/sunrise.svg'/>
-                        <Text fontSize={18}>Sunrise</Text>
+                    <Flex justifyContent='space-between' alignItems='center'>
+                        <Image src='/sunrise.svg' h='46px' />
+                        <Flex flexDir='column'>
+                            <Text textAlign='end' fontSize={18}>Sunrise</Text>
+                            <Text textAlign='end' fontSize={24}>06.13</Text>
+                        </Flex>
                     </Flex>
-                    <Text textAlign='end' fontSize={24}>06.16</Text>
                 </GridItem>
                 <GridItem boxSizing='border-box' width="100%" textColor="white" m='0px 0px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-                    <Flex alignItems='center' justifyContent='space-between'>
-                        <Image src='/sunset.svg'/>
-                        <Text fontSize={18}>Sunset</Text>
+                    <Flex justifyContent='space-between' alignItems='center'>
+                        <Image src='/sunset.svg' h='46px' />
+                        <Flex flexDir='column'>
+                            <Text textAlign='end' fontSize={18}>Sunset</Text>
+                            <Text textAlign='end' fontSize={24}>21.44</Text>
+                        </Flex>
                     </Flex>
-                    <Text textAlign='end' fontSize={24}>20.23</Text>
                 </GridItem>
                 <GridItem boxSizing='border-box' width="100%" textColor="white" m='0px 0px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-                    <Flex alignItems='center' justifyContent='space-between'>
-                        <Image src='/uv.svg'/>
-                        <Text fontSize={18}>UV</Text>
+                    <Flex justifyContent='space-between' alignItems='center'>
+                        <Image src='/uv.svg' h='46px' />
+                        <Flex flexDir='column'>
+                            <Text textAlign='end' fontSize={18}>UV</Text>
+                            <Text textAlign='end' fontSize={24}>3</Text>
+                        </Flex>
                     </Flex>
-                    <Text textAlign='end' fontSize={24}>3</Text>
                 </GridItem>
                 <GridItem boxSizing='border-box' width="100%" textColor="white" m='0px 0px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-                    <Flex alignItems='center' justifyContent='space-between'>
-                        <Image src='/wind.svg'/>
-                        <Text fontSize={18}>Max wind</Text>
+                    <Flex justifyContent='space-between' alignItems='center'>
+                        <Image src='/wind.svg' h='46px' />
+                        <Flex flexDir='column'>
+                            <Text textAlign='end' fontSize={18}>Max wind</Text>
+                            <Text textAlign='end' fontSize={24}>13.7 km/h</Text>
+                        </Flex>
                     </Flex>
-                    <Text textAlign='end' fontSize={24}>13.2 km/h</Text>
                 </GridItem>
             </Grid>
         </>
--- src/components/PageContent/Home/atoms/HomeHeader.jsx
+++ src/components/PageContent/Home/atoms/HomeHeader.jsx
@@ -3,13 +3,13 @@ import { Text, Heading, Box, Image, Flex } from '@chakra-ui/react';
 export function HomeHeader() {
     return (
         <>
-            <Heading mb='30px' fontSize='30px'>Aalborg (current location)</Heading>
+            <Heading mb='20px' fontSize='40px'>Aalborg</Heading>
             <Flex justifyContent='space-between' alignItems='center'>
                 <Box>
                     <Text fontSize='54px' display='inline' mr='10px'>16°</Text>
                     <Text fontSize='20px' display='inline'>Partly cloudy</Text>
                 </Box>
-                <Image src='/rain.svg' h='80px' />
+                <Image src='/rain.svg' h='60px' />
             </Flex>
             <Text mb='6px' fontSize='20px'>17° / 10°</Text>
             <Flex justifyContent='space-between' flexDirection='row' gap='6px' mt={4} mb={4}>
