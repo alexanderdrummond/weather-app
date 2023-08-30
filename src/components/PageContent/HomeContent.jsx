e846e7d7cc0fcdc0cb58617387a181bf31eda813
simo994q <simonkn1@hotmail.com>
2023-08-30 09:22:49 +0200

    ready to go

--- src/components/PageContent/HomeContent.jsx
+++ src/components/PageContent/HomeContent.jsx
@@ -6,18 +6,19 @@ function HomeContent() {
 
     return (
 
-        <Box p={'20px'} m={'30px'} bgColor={'gray'} borderRadius={'20px'}>
-            <Heading mb='30px' fontSize='30px'>Aalborg (current location)</Heading>
-            <Flex justifyContent='space-between' alignItems='center'>
-                <Box>
-                    <Text fontSize='54px' display='inline' mr='10px'>16°</Text>
-                    <Text fontSize='20px' display='inline'>Partly cloudy</Text>
-                </Box>
-                {/* <Image src={rainImg} h='70px' /> */}
-            </Flex>
-            <Text>17° / 10°</Text>
-        </Box>
-
+        <Flex justifyContent="center">
+            <Box width="80%" textColor="white" mt={10} p={4} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.18)">
+                <Heading mb='30px' fontSize='30px'>Aalborg (current location)</Heading>
+                <Flex justifyContent='space-between' alignItems='center'>
+                    <Box>
+                        <Text fontSize='54px' display='inline' mr='10px'>16°</Text>
+                        <Text fontSize='20px' display='inline'>Partly cloudy</Text>
+                    </Box>
+                    {/* <Image src={rainImg} h='70px' /> */}
+                </Flex>
+                <Text>17° / 10°</Text>
+            </Box>
+        </Flex>
     )
 }
 
