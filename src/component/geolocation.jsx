aa87f158aa052aec9c0e07fcd11f9ce47291f9e2
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-30 10:37:30 +0200

    update

--- src/component/geolocation.jsx
+++ src/component/geolocation.jsx
@@ -9,7 +9,7 @@ export const Geolocation = () => {
     // console.log(`Latitude : ${crd.latitude}`);
     // console.log(`Longitude: ${crd.longitude}`);
 
-    //API Request
+    // API request
     const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${crd.latitude}&lon=${crd.longitude}`;
 
     fetch(apiUrl)
@@ -17,14 +17,35 @@ export const Geolocation = () => {
       .then(data => {
         if (data.address && data.address.city) {
           const cityName = data.address.city;
-          console.log("City Name:", cityName);
+          console.log("City Name (Browser Geolocation):", cityName); // Log city name
           setCity(cityName);
         } else {
-          console.error("City name not found in response");
+          // Fallback to IP geolocation if city name not found
+          getCityFromIP();
         }
       })
       .catch(error => {
         console.error("Error fetching city name:", error);
+        // Fallback to IP geolocation in case of an error
+        getCityFromIP();
+      });
+  }
+
+  function getCityFromIP() {
+    // Make an API request to IP geolocation service
+    fetch("https://ipapi.co/json/")
+      .then(response => response.json())
+      .then(data => {
+        if (data.city) {
+          const cityName = data.city;
+          console.log("City Name (IP Geolocation):", cityName); // Log city name
+          setCity(cityName);
+        } else {
+          console.error("City name not found in IP geolocation response");
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching city name from IP:", error);
       });
   }
 
@@ -39,24 +60,29 @@ export const Geolocation = () => {
         .then(function (result) {
           console.log(result);
           if (result.state === "granted") {
-
+            // If granted then you can directly call your function here
             navigator.geolocation.getCurrentPosition(success, errors);
           } else if (result.state === "prompt") {
-
+            // If prompt then the user will be asked to give permission
             navigator.geolocation.getCurrentPosition(success, errors);
           } else if (result.state === "denied") {
-
+            // If denied then you have to show instructions to enable location
+            // Fallback to IP geolocation
+            getCityFromIP();
           }
         });
     } else {
       console.log("Geolocation is not supported by this browser.");
+      // Fallback to IP geolocation
+      getCityFromIP();
     }
   }, []);
 
   return (
     <div>
-      Location: {city}
+      Geolocation: {city} {/* Display the city name */}
     </div>
   );
 };
+
 
