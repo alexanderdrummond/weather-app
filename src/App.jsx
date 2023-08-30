c87ad62bef307b72bd2ac43e5fad908114110464
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-08-30 10:08:31 +0200

    a bit of fetching and geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Fetching } from "./component/fetch";
+
 import { Geolocation } from "./component/geolocation";
 
 
 function App() {
   return (
     <>
-      <Fetching />
+      <Fetching/>
       <Geolocation />
     </>
   );
--- src/component/fetch.jsx
+++ src/component/fetch.jsx
@@ -1,26 +1,55 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 export const Fetching = () => {
-  const [weather, setWeather] = useState([])
+  const [weather, setWeather] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
 
   const fetchWeatherData = () => {
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&daily=sunrise,sunset&timezone=Europe%2FBerlin&forecast_days=3")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setWeather(data)
-      })
-  }
+    if (userLocation) {
+      const { latitude, longitude } = userLocation;
+
+      fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max,windspeed_10m_max&timezone=Europe%2FBerlin&forecast_days=14`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeather(data);
+          console.log("Weather Data:", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+        });
+    }
+  };
 
   useEffect(() => {
-    fetchWeatherData()
-  }, [])
-  console.log("Weather", weather)
-  
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ latitude, longitude });
+
+        console.log("Location:", { latitude, longitude });
+      },
+      (error) => {
+        console.error("Error getting  location:", error);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [userLocation]);
+
   return (
     <div>
-      Test
+      {weather ? (
+        <div>
+          {/* Render weather data here */}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
-}
+};
