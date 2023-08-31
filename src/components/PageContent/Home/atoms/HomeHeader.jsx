cb0a68e4be5dc93f044ee19592c79f9a2d064f54
simo994q <simonkn1@hotmail.com>
2023-08-31 09:29:53 +0200

    small css tweaks

--- src/components/PageContent/Home/atoms/HomeHeader.jsx
+++ src/components/PageContent/Home/atoms/HomeHeader.jsx
@@ -3,16 +3,16 @@ import { Text, Heading, Box, Image, Flex } from '@chakra-ui/react';
 export function HomeHeader() {
     return (
         <>
-            <Heading mb='20px' fontSize='40'>Aalborg</Heading>
-            <Flex justifyContent='space-between' alignItems='center'>
+            <Heading mb='10px' fontSize='44' fontWeight='500'>Aalborg</Heading>
+            <Flex justifyContent='space-between' alignItems='center' mb='6px'>
                 <Box>
                     <Text fontSize='54px' display='inline' mr='10px'>16°</Text>
                     <Text fontSize='20px' display='inline'>Partly cloudy</Text>
                 </Box>
                 <Image src='/rain.svg' h='60px' />
             </Flex>
-            <Text mb='6px' fontSize='20px'>17° / 10°</Text>
-            <Flex justifyContent='space-between' flexDirection='row' gap='6px' mt={4} mb={4}>
+            <Text mb='16px' fontSize='20px'>17° / 10°</Text>
+            <Flex justifyContent='space-between' flexDirection='row' gap='6px' mb='16px'>
                 <Flex alignItems='center' gap='10px'>
                     <Image src='/wind.svg' h='26px' />
                     <Text>9 km/h</Text>
