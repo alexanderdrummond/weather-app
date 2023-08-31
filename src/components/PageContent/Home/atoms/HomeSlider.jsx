b822e1f1b67c50bf0e2fb08efd605da1e15cd549
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-08-31 14:59:33 +0200

    Weather slider things

--- src/components/PageContent/Home/atoms/HomeSlider.jsx
+++ src/components/PageContent/Home/atoms/HomeSlider.jsx
@@ -3,21 +3,30 @@ import { mapCode } from '../../../../utils/weatherUtils';
 
 export function HomeSlider({ weatherInfo, isLoading }) {
 
-    const { weatherData, cityName } = weatherInfo;
+    const { weatherData } = weatherInfo;
 
     const d = new Date();
     let currentTimeIndex = d.getHours();
     
     const { icon: weatherIcon, description: weatherDescription } = mapCode(weatherData?.hourly.weathercode[currentTimeIndex])
 
+    const currentHour = new Date().getHours();
+
 
     return (
         <Flex overflow='scroll' boxSizing='border-box' width="100%" textColor="white" mb='16px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src={weatherIcon} height='30px' alt={weatherDescription} />
-                <Text>{Math.round(weatherData?.hourly.temperature_2m[0])}°</Text>
-            </Flex>
+            {weatherData?.hourly.temperature_2m.slice(currentHour, currentHour + 25).map((temperature, index) => {
+                const weatherCode = weatherData?.hourly.weathercode[index];
+                const { icon: weatherIcon } = mapCode(weatherCode);
+                const displayHour = (currentHour + index) % 24;
+                return (
+                    <Flex key={index} flexDir='column' alignItems='center' gap='6px'>
+                        <Text>{displayHour}:00</Text>
+                        <Image src={weatherIcon} height='30px' alt='weather icon' />
+                        <Text>{Math.round(temperature)}°</Text>
+                    </Flex>
+                );
+            })}
         </Flex>
-    )
+    );
 }
