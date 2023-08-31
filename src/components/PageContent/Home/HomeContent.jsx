feaa6ea1455ea9e71787743b1bdfad6a679e8362
simo994q <simonkn1@hotmail.com>
2023-08-31 10:08:48 +0200

    animation

--- src/components/PageContent/Home/HomeContent.jsx
+++ src/components/PageContent/Home/HomeContent.jsx
@@ -2,18 +2,24 @@ import { Box, Flex } from '@chakra-ui/react';
 import { HomeHeader } from "./atoms/HomeHeader"
 import { HomeSlider } from './atoms/HomeSlider';
 import { HomeFacts } from './atoms/HomeFacts';
+import { motion } from 'framer-motion';
 
 function HomeContent() {
 
 
     return (
-        <Flex justifyContent="center">
-            <Box width="80%" textColor="white" mt={10} p={4} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.18)">
-                <HomeHeader />
-                <HomeSlider />
-                <HomeFacts />
-            </Box>
-        </Flex>
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}>
+            <Flex justifyContent="center">
+                <Box width="80%" textColor="white" mt={10} p={4} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.18)">
+                    <HomeHeader />
+                    <HomeSlider />
+                    <HomeFacts />
+                </Box>
+            </Flex>
+        </motion.div>
     )
 }
 
--- src/components/PageContent/Home/atoms/HomeSlider.jsx
+++ src/components/PageContent/Home/atoms/HomeSlider.jsx
@@ -2,62 +2,62 @@ import { Text, Heading, Box, Image, Flex } from '@chakra-ui/react';
 
 export function HomeSlider() {
     return (
-        <Flex overflow='scroll'boxSizing='border-box' width="100%" textColor="white" mb='16px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
+            <Flex overflow='scroll' boxSizing='border-box' width="100%" textColor="white" mb='16px' p={3} backgroundColor="rgba(255, 255, 255, 0.1)" borderRadius="10px" backdropFilter="blur(13.5px)" border="1px solid rgba(255, 255, 255, 0.28)" gap='30px'>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
+                <Flex flexDir='column' alignItems='center' gap='6px'>
+                    <Text>Now</Text>
+                    <Image src='/rain.svg' height='30px' />
+                    <Text>16°</Text>
+                </Flex>
             </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-            <Flex flexDir='column' alignItems='center' gap='6px'>
-                <Text>Now</Text>
-                <Image src='/rain.svg' height='30px'/>
-                <Text>16°</Text>
-            </Flex>
-        </Flex>
     )
 }
